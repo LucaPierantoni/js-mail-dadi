18f56bcba34ebfa1baa7edefc43dfc5315cec07c
LucaPierantoni <luca.pier99@gmail.com>
2024-02-01 18:23:21 +0100

    esercizio mail completo

--- mail/js/script.js
+++ mail/js/script.js
@@ -4,28 +4,47 @@
 // (NON utilizzate il metodo .includes())
 
 const accessOut = document.getElementById("access");
+const inputMail = document.getElementById("user-email");
+const verifyBtn = document.getElementById("verify");
 
 
-/* creo un'array con 5 email */
-const mailList = ["luca@gmail.com", "mauro@gmail.com", "paola@gmail.com", "roberto@gmail.com", "gianni@gmail.com"];
+const lock = document.querySelector(".locked");
+const unlock = document.querySelector(".unlocked");
 
-/* chiedo all'utente la sua mail */
-const userMail = prompt("Inserisci la tua email per il controllo");
 
-let accessoConsentito = false;
 
-/* creo un ciclo di controllo */
+verifyBtn.addEventListener("click",
+    function(){
+        event.preventDefault(); // Impedisce il comportamento predefinito del modulo di invio
+        /* creo un'array con 5 email */
+        const mailList = ["luca@gmail.com", "mauro@gmail.com", "paola@gmail.com", "roberto@gmail.com", "gianni@gmail.com"];
 
-for (i=0; i<mailList.length; i++){
+        const userMail = inputMail.value;
 
-    /* se l'email dell'utente e uguale a una delle mail inserite nell'array */
-    if (userMail === mailList[i]){
-        accessoConsentito = true;
+        let accessoConsentito = false; 
+
+        const lock = document.querySelector(".locked");
+        const unlock = document.querySelector(".unlocked");
+
+
+        for (i=0; i<mailList.length; i++){
+            /* se l'email dell'utente e uguale a una delle mail inserite nell'array */
+            if (userMail === mailList[i]){
+                accessoConsentito = true;
+            }
+        }
+        if (accessoConsentito === true) {
+            accessOut.innerHTML = 'Accesso Consentito';
+            accessOut.classList.add("green");
+            lock.classList.add("none");
+            unlock.classList.remove("none");
+        } else {
+            accessOut.innerHTML = 'Accesso Negato';
+            accessOut.classList.add("red");
+            accessOut.classList.remove("green");
+            unlock.classList.add("none");
+            lock.classList.remove("none");
+        }
+        console.log(inputMail.value);
     }
-}
-
-if (accessoConsentito === true) {
-    accessOut.innerHTML = 'Accesso Consentito';
-} else {
-    accessOut.innerHTML = 'Accesso Negato';
-}
+)
